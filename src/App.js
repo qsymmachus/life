b5b1c992783b879079ffc5f71721ab5b7a5ffe42
John Olmsted <john@olmsted.io>
2017-09-23 14:45:15 -0700

    Adds Cell component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import Cell from './components/cell/Cell'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Conway's Game of Life</h1>
+        <Cell alive={true}/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/cell/Cell.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Cell.css'
+
+/** A cell in the game of life. A cell can either be alive or dead. */
+class Cell extends Component {
+  get style() {
+    return {
+      backgroundColor: this.props.alive ? "black" : "white"
+    }
+  }
+
+  render() {
+    return (
+      <div className="cell" style={this.style}>
+      </div>
+    )
+  }
+}
+
+export default Cell
