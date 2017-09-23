5258fc78704e616b0adcfb9c184908ab4c285704
John Olmsted <john@olmsted.io>
2017-09-23 15:55:18 -0700

    Fix issue where there was a gap between cell rows

---
+++ src/components/row/Row.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+/** A row for cells. Styled to hold 100 cells on a single row. */
+class Row extends Component {
+  render() {
+    return (
+      <div className="row">
+      </div>
+    )
+  }
+}
+
+export default Row
