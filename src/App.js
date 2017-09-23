8d927f4401522611798eb314fc535f295088e839
John Olmsted <john@olmsted.io>
2017-09-23 14:49:32 -0700

    Centers the App component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import Cell from './components/cell/Cell'
 import './App.css'
 
+/** The root component of our application. */
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <h1>Conway's Game of Life</h1>
         <Cell alive={true}/>
       </div>
