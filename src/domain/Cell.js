3e759ae9ae0297a3ec55c0aeb398341f1c3c600e
John Olmsted <john@olmsted.io>
2017-10-07 15:52:51 -0700

    Extracts 'randomBoolean()' into a helper file

--- src/domain/Cell.js
+++ src/domain/Cell.js
@@ -1,4 +1,5 @@
 import Position from './Position'
+import { randomBoolean } from './helpers/Random'
 
 /** 
  * Represents a Cell in the game of life. 
@@ -15,11 +16,6 @@ class Cell {
 /** Generates a Cell with a random initial state. */
 Cell.generate = function(position = new Position(0, 0)) {
   return new Cell(randomBoolean(), position)
-}
-
-/** Helper function that returns a randomly generated boolean. */
-function randomBoolean() {
-  return Math.random() >= 0.5;
 }
 
 export default Cell
---
+++ src/domain/helpers/Random.js
@@ -0,0 +1,4 @@
+/** Helper function that returns a randomly generated boolean. */
+export function randomBoolean() {
+  return Math.random() >= 0.5;
+}
