268481db978c6615e00d0dd54821d67eb44bb858
John Olmsted <john@olmsted.io>
2017-10-13 14:33:10 -0700

    Adds GridContainer

---
+++ src/containers/GridContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { connect } from 'redux-zero'
+
+import GridComponent from '../../components/Grid'
+
+const mapStateToProps = ({ grid }) => ({ grid })
+
+/** Container component that maps application state to the Grid component props. */
+export default connect(mapStateToProps)(({ grid }) => (
+  <GridComponent grid={grid}/>
+))
