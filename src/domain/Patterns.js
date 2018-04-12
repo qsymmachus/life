eceaf72f24c630eaf29cd2d94e4b4ec5d3009195
John Olmsted <john@olmsted.io>
2018-04-11 18:27:38 -0700

    Adds gosper glider gun pattern
    Adds a 'Patterns' object which stores reusable grid patterns. Adds the
    'gosper glider gun' as an initial pattern.

---
+++ src/domain/Patterns.js
@@ -0,0 +1,49 @@
+import { padBooleanArray } from './helpers/Padder'
+
+/**
+ * Some established Game of Life patterns that we can use as the initial state for a grid.
+ * 
+ * Each pattern is represented as a two-dimensional array of booleans. A `true` cell is a live cell.
+ * Each pattern is 100 x 100 cells (even if the underlying pattern is smaller).
+ * 
+ * To use a pattern, instantiate a Grid with it:
+ * 
+ *   Grid.generateFromBooleans(Patterns.somePattern())
+ * 
+ */
+let Patterns = {
+  /** Given a plaintext pattern string, converts it to a two-dimensional array of booleans. */
+  fromString: function(patternString) {
+    let patternArray = patternString
+      .split("\n")
+      .filter((line) => line !== '')
+      .map((line) => 
+        line.split('').map((char) => char === 'O' ? true : false)
+      )
+      
+    return padBooleanArray(patternArray)
+  },
+
+  /** Pattern string for a gosper glider gun. */
+  gosperGliderGunString: `
+........................O
+......................O.O
+............OO......OO............OO
+...........O...O....OO............OO
+OO........O.....O...OO
+OO........O...O.OO....O.O
+..........O.....O.......O
+...........O...O
+............OO
+`,
+
+  /** 
+   * Pattern for a gosper glider gun.
+   * See: http://www.conwaylife.com/w/index.php?title=Gosper_glider_gun 
+   */
+  gosperGliderGun: function() {
+    return this.fromString(this.gosperGliderGunString)
+  }
+}
+
+export default Patterns
